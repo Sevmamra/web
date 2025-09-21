1bb636fd3e9b5f4c27baa0146e94524da184dcc6
Sevmamra <sevmamra01@gmail.com>
2025-09-21 09:58:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const message = document.getElementById("message");
+  message.textContent = "JavaScript updated this text dynamically!";
+});
